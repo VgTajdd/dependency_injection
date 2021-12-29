6af23f4cabec568a4faa9d230d8e2c5c4f429c9b
VgTajdd <aduranddiaz@gmail.com>
2021-12-29 12:07:02 -0500

    Add code to make the tests pass

--- include/pkg/pkg.h
+++ include/pkg/pkg.h
@@ -5,3 +5,97 @@ void printHelloWorld();
 void printNumber( int number );
 
 int getFibonacci( int n );
+
+/////////////////////////////////////////////////////////////
+// TODO:
+// REGISTER SINGLETON SERVICE
+// REGISTER TRANSIENT SERVICE
+// SERVICES WITH PARAMETERS (AUTORESOLVE) IN RUNTIME
+
+#include <typeinfo>
+#include <typeindex>
+#include <map>
+#include <iostream>
+#include <functional>
+
+struct Service
+{
+	virtual ~Service() = default;
+	virtual void run() = 0;
+};
+
+struct ServiceImpl : Service
+{
+	void run() override
+	{
+		std::cout << "ServiceImpl::run()" << std::endl;
+	}
+};
+
+struct ServiceDefinition
+{
+	std::type_info const& type_info;
+	std::type_index type_index;
+	std::function<void* ( )> factory;
+};
+
+static std::map<std::type_index, ServiceDefinition> services;
+
+template<class TInterface, class TImplementation, typename... TArgs>
+void RegisterService( TArgs... args )
+{
+	static_assert( std::is_base_of<TInterface, TImplementation>::value, "TImplementation must be derived from T" );
+	const auto& typeInterface = typeid( TInterface );
+	auto factory = [=]() -> TInterface* { return new TImplementation( args... ); };
+	services.emplace( std::type_index( typeInterface ), ServiceDefinition{ typeInterface, typeInterface, factory } );
+}
+
+template<class TInterface>
+TInterface* GetService()
+{
+	const auto& typeInterface = typeid( TInterface );
+	auto serviceDefinition = services.find( std::type_index( typeInterface ) );
+	if ( serviceDefinition == services.end() )
+	{
+		return nullptr;
+	}
+	else
+	{
+		return static_cast<TInterface*>( serviceDefinition->second.factory() );
+	}
+}
+
+class IPowerOfTwoGenerator
+{
+public:
+	virtual int value() = 0;
+};
+
+class PowerOfTwoGenerator : public IPowerOfTwoGenerator
+{
+public:
+	int value() override
+	{
+		return 2;
+	}
+};
+
+class IPowerOfThreeGenerator
+{
+public:
+	virtual int value() = 0;
+};
+
+class PowerOfThreeGenerator : public IPowerOfThreeGenerator
+{
+public:
+	PowerOfThreeGenerator( int power )
+	{
+		this->power = power;
+	}
+	int value() override
+	{
+		return pow( 3, this->power );
+	}
+	int power;
+};
