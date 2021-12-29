8b575ff4f5896fa678f0b72ed9906a4902b04b90
VgTajdd <aduranddiaz@gmail.com>
2021-12-29 12:06:31 -0500

    Add some tests (fail)
    Use TDD

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,3 +6,19 @@ TEST( TemplateTest, Test1 )
 {
 	EXPECT_EQ( getFibonacci( 5 ), 5 );
 }
+
+TEST( TemplateTest, Test2 )
+{
+	RegisterService<IPowerOfTwoGenerator, PowerOfTwoGenerator>();
+	const auto& service{ GetService<IPowerOfTwoGenerator>() };
+
+	EXPECT_EQ( service->value(), 2 );
+}
+
+TEST( TemplateTest, Test3 )
+{
+	RegisterService<IPowerOfThreeGenerator, PowerOfThreeGenerator, int>( 2 );
+	const auto& service{ GetService<IPowerOfThreeGenerator>() };
+
+	EXPECT_EQ( service->value(), 9 );
+}
