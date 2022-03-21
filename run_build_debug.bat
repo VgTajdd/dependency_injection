82af1e2b55e9f18a1b44b455243447148ad7d292
VgTajdd <aduranddiaz@gmail.com>
2022-03-21 08:01:52 -0500

    Update run_build_debug script (bat & sh)

--- run_build_debug.bat
+++ run_build_debug.bat
@@ -1,6 +1,5 @@
 @echo off
 
-::del build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug -j
 cd build/Debug/
--- run_build_debug.sh
+++ run_build_debug.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#rm -rf build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --config Debug -j
 cd build
