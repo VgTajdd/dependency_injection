c4f7688e7675d018504403853e65617cb356ec61
VgTajdd <aduranddiaz@gmail.com>
2022-03-23 07:55:20 -0500

    Update src/source.cpp

--- src/source.cpp
+++ src/source.cpp
@@ -5,10 +5,21 @@
 // ========================================================================= //
 
 #include "pkg/pkg.h"
+#include "pkg/DIContainer.h"
+
+#include "ServicesInterfaces.h"
+#include "ServicesImplementations.h"
+#include "ClientClass.h"
 
 int main( int, char** )
 {
 	printHelloWorld();
 	printNumber( getFibonacci( 5 ) );
+
+	DIContainer container;
+	container.registerService<IPowerOfTwoGenerator, PowerOfTwoGenerator>();
+	const auto& service{ container.getService<IPowerOfTwoGenerator>() };
+	std::cout << service->value() << std::endl;
+
 	return 0;
 }
